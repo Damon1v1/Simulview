ad411ac11d68a714ac88dde730727ae19eb1dacb
Clint <maxeva02@gmail.com>
2021-07-04 16:20:12 -0700

    began creating the folders for the chatroom area

---
+++ Client/src/components/header/title.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./title.css";
+
+function Header() {
+    return (
+      <header>
+          <h1>Simulview</h1>
+      </header>
+    );
+  }
+  
+  export default Header;
