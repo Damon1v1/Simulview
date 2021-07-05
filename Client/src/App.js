9580abc05d02c85c6b723067a6b8b77ea8d1b59a
Clint <maxeva02@gmail.com>
2021-07-05 14:59:35 -0700

    working on the navbar, header, footer, background image done

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,10 +1,29 @@
 import './App.css';
+import background from './images/background.jpg'
+import { Navigation } from 'react';
+import { Link } from 'react-router-dom';
 import Header from './components/header/title'
+import Footer from './components/footer/footer'
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
+    <div
+      class='bg_image'
+        style={{
+          backgroundImage: 'url('+background+')',
+          backgroundSize: 'cover',
+          height: '100vh',
+          marginTop: '-25px'
+        }}
+    >
+      <Header style={{color:'white'}} className="header-color" title={<Link style={{textDecoration: 'none', color: 'white'}} to="/">Simulview</Link>}>
+          <Navigation>
+              <Link to="/loginPage">Login</Link>
+              <Link to="/profile">Profile</Link>
+          </Navigation>
+      </Header>
+      <Footer />
     </div>
   );
 }
