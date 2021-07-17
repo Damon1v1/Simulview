d4d231f22bba884a57f5cf48fab94753ef7c4d2f
Clint <maxeva02@gmail.com>
2021-07-17 10:32:31 -0700

    messaging forms

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,20 +1,24 @@
 import { ChatEngine } from 'react-chat-engine';
 
-//import ChatFeed from './components/ChatFeed';
-//import LoginForm from './components/LoginForm';
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
+const projectID = 'b4198e88-d501-49d4-8fd8-6fc0a4d41981';
+
 const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
     <ChatEngine
       height="100vh"
-      projectID='b4198e88-d501-49d4-8fd8-6fc0a4d41981'
-      userName='simulview123'
-      userSecret='123'
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
     />
   );
 };
-
-// infinite scroll, logout, more customizations...
 
 export default App;
